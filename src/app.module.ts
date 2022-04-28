f3e7b234c54cd7e2270c8087322db7ac8175b53f
Eren <erennakdag10@gmail.com>
2022-04-28 20:03:27 +0200

    created therapist provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PatientsModule } from './patient/patient.module';
+import { TherapistModule } from './therapist/therapist.module';
 
 @Module({
-  imports: [PatientsModule],
+  imports: [PatientsModule, TherapistModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/therapist/therapist.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma, Therapist } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class TherapistService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getTherapists(): Promise<Therapist[]> {
+    return await this.prisma.therapist.findMany();
+  }
+
+  async getTherapistById(id: string): Promise<Therapist> {
+    return await this.prisma.therapist.findUnique({ where: { id } });
+  }
+
+  async getTherapistByFilters(
+    where: Prisma.TherapistWhereInput,
+  ): Promise<Therapist[]> {
+    return await this.prisma.therapist.findMany({ where });
+  }
+
+  async createTherapist(data: Prisma.TherapistCreateInput): Promise<Therapist> {
+    return await this.prisma.therapist.create({ data });
+  }
+
+  async updateTherapist({
+    where,
+    data,
+  }: {
+    where: Prisma.TherapistWhereUniqueInput;
+    data: Prisma.TherapistUpdateInput;
+  }): Promise<Therapist> {
+    return await this.prisma.therapist.update({ where, data });
+  }
+}
